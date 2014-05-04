38f8a7db2fcdb3ad7fb8864726bf1a9ed8be562e
Alejandro Pineda <alejandro.deathscythe@gmail.com>
2014-05-04 14:32:02 -0500

    Signup

--- signups/admin.py
+++ signups/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import signUp
+
+class signUpAdmin(admin.ModelAdmin):
+	class Meta:
+		model = signUp
+
+admin.site.register(signUp, signUpAdmin)
