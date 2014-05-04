6d8a7350cad43fe2062249e0e3e53f17ee338259
Alejandro Pineda <alejandro.deathscythe@gmail.com>
2014-05-04 15:37:25 -0500

    Template Dir

--- mvp_landing/settings.py
+++ mvp_landing/settings.py
@@ -81,3 +81,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#Template location
+
+TEMPLATE_DIRS = (os.path.dirname(BASE_DIR), 'static', 'template')
